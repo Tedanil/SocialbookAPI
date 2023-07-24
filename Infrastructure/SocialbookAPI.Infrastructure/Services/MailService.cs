bc8b1dd79bafd2238636f8aec97e6a452a95487e
Tedanil <anildayi@hotmail.com>
2023-07-24 14:48:29 +0300

    Created MailService

---
+++ Infrastructure/SocialbookAPI.Infrastructure/Services/MailService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using SocialbookAPI.Application.Abstractions.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialbookAPI.Infrastructure.Services
+{
+    public class MailService : IMailService
+    {
+        readonly IConfiguration _configuration;
+
+        public MailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendMailAsync(string to, string subject, string body, bool isBodyHtml = true)
+        {
+            await SendMailAsync(new[] { to }, subject, body, isBodyHtml);
+        }
+
+        public async Task SendMailAsync(string[] tos, string subject, string body, bool isBodyHtml = true)
+        {
+            MailMessage mail = new();
+            mail.IsBodyHtml = isBodyHtml;
+            foreach (var to in tos)
+                mail.To.Add(to);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.From = new(_configuration["Mail:Username"], "ERBAS E-Ticaret", System.Text.Encoding.UTF8);
+
+            SmtpClient smtp = new();
+            smtp.Credentials = new NetworkCredential(_configuration["Mail:Username"], _configuration["Mail:Password"]);
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.Host = _configuration["Mail:Host"];
+            await smtp.SendMailAsync(mail);
+        }
+
+        public async Task SendPasswordResetMailAsync(string to, string userId, string resetToken)
+        {
+            string mail = "Merhaba<br>Eğer yeni şifre talebinde bulunduysanız aşağıdaki linkten şifrenizi yenileyebilirsiniz.<br><strong><a href='" + _configuration["AngularClientUrl"] + "/update-password/" + userId + "/" + resetToken + "' target='_blank'>Yeni şifre talebi için tıklayınız...</a></strong><br><br><span style='font-size:12px;'>NOT : Eğer ki bu talep tarafınızca gerçekleştirilmemişse lütfen bu maili ciddiye almayınız.</span><br>Saygılarımızla...<br><br><br>NG - ERBAS|E-Ticaret";
+            await SendMailAsync(to, "Şifre Yenileme Talebi", mail);
+        }
+    }
+}
